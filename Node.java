5507ceadf06f8e8ecaec09169e5cdbd9bfaeb32e
thiagograsso <143461239+thiagograsso@users.noreply.github.com>
2024-09-04 21:18:10 -0300

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,13 @@
+public class Node {
+    int info;
+    Node esquerda;
+    Node direita;
+
+    public Node(int info) {
+        this.info = info;
+        this.esquerda = null;
+        this.direita = null;
+    }
+
+    // Getters e setters podem ser adicionados aqui, se necessário
+}
